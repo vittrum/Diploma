3c82455cd54e569adce84f564fc24e10010adf08
vittrum <viktorkovalskyi9@gmail.com>
2020-01-04 20:40:11 +0200

    basics

--- Genetic/Genetic/Genetic.cpp
+++ Genetic/Genetic/Genetic.cpp
@@ -4,15 +4,62 @@
 #include <iostream>
 #include <vector>
 #include <chrono> 
-
+#include <tuple>
+#include <array>
 using namespace std;
-using namespace std::chrono;
+using namespace std::chrono; // Clock
 
-// Use auto keyword to avoid typing long 
-// type definitions to get the timepoint 
-// at this instant use function now() 
-auto start = high_resolution_clock::now();
 
+// TO DO:
+// Дебажить заполнение координат
+// Определить размещение контейнеров
+// Прикрутить библиотеку генетических алгоритмов
+
+
+auto start = high_resolution_clock::now();	// Clock
+
+// Заполнить трюм пустыми значениями
+auto initialize_hold(vector<vector<int>> hold, int a_side, int b_side)->vector<vector<int>>;
+// Заполнить список контейнеров значениями
+auto fill_containers()->vector<tuple<int, int>>;
+// Создать список возможных координат для каждого контейнера
+auto create_container_coordinates(vector<tuple<int, int>> containers, vector<vector<int>> hold)->vector<array<int, 4>>;
+// Разместить контейнеры
+auto settle_containers_in_hold();
+
+int main()
+{
+	auto start = high_resolution_clock::now();	// Clock
+
+	vector<vector<int>> hold;
+	int a = 5; // Vertical
+	int b = 3; // Horizontal
+	hold = initialize_hold(hold, a, b);
+	auto containers = fill_containers();
+
+	// Распечатать трюм
+	/*
+	for (int i = 0; i < a; i++)
+	{
+		for (int j = 0; j < b; j++)
+			cout << hold[i][j] << ' ';
+		cout << endl;
+	}*/
+
+	auto coordinates = create_container_coordinates(containers, hold);
+	// Распечатать координаты
+	for (int i = 0; i < coordinates.size(); i++)
+	{
+		cout << endl;
+		for (int j = 0; j < coordinates[0].size(); j++)
+			cout << coordinates[i][j] << ' ';
+	}
+
+	auto stop = high_resolution_clock::now();					// Clock
+	auto duration = duration_cast<microseconds>(stop - start);	// Clock
+
+	cout << endl << duration.count() << " ms" << endl;			// Clock
+}
 
 vector<vector<int>> initialize_hold(vector<vector<int>> hold, int a_side, int b_side)
 {
@@ -25,36 +72,36 @@ vector<vector<int>> initialize_hold(vector<vector<int>> hold, int a_side, int b_
 	return hold;
 }
 
-int main()
+vector<tuple<int, int>> fill_containers()
 {
-	auto start = high_resolution_clock::now();
-
-	vector<vector<int>> hold;
-	int a = 5;
-	int b = 3;
-	hold = initialize_hold(hold, a, b);
-
-	for (int i = 0; i < a; i++)
-	{
-		for (int j = 0; j < b; j++)
-			cout << hold[i][j] << ' ';
-		cout << endl;
-	}
-
-	auto stop = high_resolution_clock::now();
-	auto duration = duration_cast<microseconds>(stop - start);
-
-	cout << duration.count() << " ms" << endl;
+	vector<tuple<int, int>> containers;
+	auto small = make_tuple(2, 2); 
+	auto med = make_tuple(2, 3);
+	auto big = make_tuple(2, 4);
+	containers.push_back(small);
+	containers.push_back(med);
+	containers.push_back(big);
+	return containers;
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+vector<array<int, 4>> create_container_coordinates(vector<tuple<int, int>> containers, vector<vector<int>> hold)
+{
+	vector<array<int, 4>> coordinates;
+	int a = hold.size();
+	int b = hold[0].size();
+	array <int, 4> coord;
+	for (auto cont : containers) 
+	{
+		for (int i = 0; i < a - get<1>(cont) + 1; i++)
+			for (int j = 0; j < b - get<0>(cont) + 1; j++)
+			{
+				coord = { i, j, i + a, j + b };
+				coordinates.push_back(coord);
+			}
+	}
+	return coordinates;
+}
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+
+
 
